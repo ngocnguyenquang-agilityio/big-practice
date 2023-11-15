835a1a0e24fb11c240d4c73a191e63c058436412
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-11-15 15:19:56 +0700

    feat: #146 add code comment

--- practices/big-practice/src/hooks/useClickOutSide.ts
+++ practices/big-practice/src/hooks/useClickOutSide.ts
@@ -1,6 +1,11 @@
 // Libs
 import { useEffect, useRef } from 'react';
 
+/**
+ * Custom hook to detect user click outside component or not
+ * @param {Function} callback
+ * @returns
+ */
 export const useOutsideClick = (callback: () => void) => {
   const ref = useRef<HTMLElement>(null);
 
